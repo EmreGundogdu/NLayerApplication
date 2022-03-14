277d9f7956da6e74fce6f2cc06e7c298a2cd9b85
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-03-14 23:46:04 +0300

    Seed

--- NLayer.Repository/AppDbContext.cs
+++ NLayer.Repository/AppDbContext.cs
@@ -21,11 +21,15 @@ namespace NLayer.Repository
         public DbSet<Product> Products { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            
+
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly()); //bunu yazarsak diğer configurationları yazmamıza gerek yok çalıştığımız yerdeki bütün configurationları getiricek ve uygulayacak
 
             //modelBuilder.ApplyConfiguration(new CategoryConfiguration());
             //modelBuilder.ApplyConfiguration(new ProductFeatureConfiguration());
+
+            modelBuilder.Entity<ProductFeature>().HasData(
+                new ProductFeature() { Id = 1, Color = "Kırmızı", Height = 100, Width = 200, ProductId = 1 },
+                new ProductFeature() { Id = 2, Color = "Mavi", Width = 50, Height = 200, ProductId = 2 });
         }
     }
 }
---
+++ NLayer.Repository/Seeds/ProductSeed.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Seeds
+{
+    public class ProductSeed : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasData(
+                new Product { Id = 1, Name = "Kurşun Kalem", CategoryId = 1, Price = 5, Stock = 20, CreatedDate = DateTime.Now },
+                new Product { Id = 2, Name = "Güzel Yazı Defteri", CategoryId = 2, Price = 15, Stock = 10, CreatedDate = DateTime.Now },
+                new Product { Id = 3, Name = "Almanca Dil Bilgisi Kitabı", CategoryId = 3, Price = 90, Stock = 10, CreatedDate = DateTime.Now });
+        }
+    }
+}
