0aae7b9c1f24c70ebdb908d4e1862a33f24d0da4
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-03-16 14:31:52 +0300

    CustomResponseDTO added

--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -1,10 +1,13 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using NLayer.Core.IUnitOfWorks;
 using NLayer.Core.Repositories;
 using NLayer.Core.Services;
 using NLayer.Repository;
 using NLayer.Repository.Repositories;
 using NLayer.Repository.UnitOfWork;
+using NLayer.Service.Mapping;
+using NLayer.Service.Services;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,9 +18,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-//builder.Services.AddScoped(typeof(IService<>), typeof(Service<>));
+builder.Services.AddScoped(typeof(IService<>), typeof(Service<>));
+builder.Services.AddAutoMapper(typeof(MapProfile));
+
 builder.Services.AddDbContext<AppDbContext>(p =>
 {
     p.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"), opt =>
