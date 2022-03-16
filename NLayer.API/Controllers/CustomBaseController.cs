50c52e785376e6247df0978ae664e13afd8a9c19
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-03-16 19:07:45 +0300

    API Controllers

---
+++ NLayer.API/Controllers/CustomBaseController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.DTOs;
+
+namespace NLayer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomBaseController : ControllerBase
+    {
+        public IActionResult CreateActionResult<T>(CustomResponseDTO<T> response)
+        {
+            if(response)
+        }
+    }
+}
---
+++ NLayer.API/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.DTOs;
+using NLayer.Core.Model;
+using NLayer.Core.Services;
+
+namespace NLayer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        readonly IMapper _mapper;
+        readonly IService<Product> _service;
+
+        public ProductsController(IMapper mapper, IService<Product> service)
+        {
+            _mapper = mapper;
+            _service = service;
+        }
+        public async Task<IActionResult> All()
+        {
+            var products = await _service.GetAllAsync();
+            var productsDtos = _mapper.Map<List<ProductDTO>>(products.ToList());
+            return Ok(CustomResponseDTO<List<ProductDTO>>.Success(200, productsDtos));
+        }
+    }
+}
